3d73b190e000459b5d2d34c68fb86145364cf7ff
erickaldiaz@gmail.com <dificil1>
2020-09-02 14:24:01 -0300

    change excercise

--- ejercicio-funciones1/src/ejercicio-funciones1.c
+++ ejercicio-funciones1/src/ejercicio-funciones1.c
@@ -7,11 +7,107 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
+/*pedir al usuario dos numeros float
+ * función sumar
+ * función restar
+ * función multiplicar
+ * función dividir
+ *
+ * todas las funciones en casp de exito return 0 y en caso de error return -1
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+int suma(int numA, int numB);
+int resta(int numA, int numB);
+int multiplicar(int numA, int numB);
+int dividir(int operadoA, int numB, float *resultado);
+
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	setbuf(stdout, NULL);
+	float numUno;
+	float numDos;
+	float resultadoSuma;
+	float resultadoResta;
+	float resultadoMultiplicacion;
+	float resultadoDivision;
+
+
+
+	if(getFloat("\nIngrese el primer operando: ","\nError, Ingrese un numero: ",2,&numUno) == 0)
+	{
+		if(getFloat("\nIngrese el segundo operando: ","\nError, Ingrese un numero: ",2,&numDos) == 0)
+		{
+			if (sumar(numUno, numDos, &resultadoSuma) == 0) {
+					printf("\nEl resultado de la suma es: %.2f", resultadoSuma);
+					fflush(stdin);
+				} else {
+				    printf("\nAlgo salio mal");
+				    fflush(stdin);
+				}
+				if (restar(numUno, numDos, &resultadoResta) == 0) {
+					printf("\nEl resultado de la resta es: %.2f", resultadoResta);
+					fflush(stdin);
+				} else {
+				    printf("\nAlgo salio mal");
+				}
+				if (dividir(numUno, numDos, &resultadoDivision) == 0) {
+					printf("\nEl resultado de la division es: %.2f", resultadoDivision);
+				} else {
+					printf("\nNo se puede dividir por 0");
+					fflush(stdin);
+				}
+				if (multiplicar(numUno, numDos, &resultadoMultiplicacion) == 0) {
+					printf("\nEl resultado de la multiplicacion es: %.2f",
+							resultadoMultiplicacion);
+					fflush(stdin);
+				}else {
+					printf("\nAlgo salio mal");
+					fflush(stdin);
+				}
+		}
+
+	}
+
 	return EXIT_SUCCESS;
+}
+
+int sumar(float operadorA, float operadorB, float *pResultado) {
+
+	int retorno = -1;
+
+	*pResultado = operadorA + operadorB;
+	retorno = 0;
+
+	return retorno;
+}
+int restar(float operadorA, float operadorB, float *pResultado) {
+
+	int retorno = -1;
+
+	*pResultado = operadorA - operadorB;
+	retorno = 0;
+
+	return retorno;
+}
+int multiplicar(float operadorA, float operadorB, float *pResultado) {
+
+	int retorno = -1;
+
+	*pResultado = operadorA * operadorB;
+	retorno = 0;
+
+	return retorno;
+}
+int dividir(float operadorA, float operadorB, float *pResultado) {
+
+	int retorno = -1;
+
+	if (operadorB != 0) {
+		*pResultado = operadorA / operadorB;
+		retorno = 0;
+	}
+
+	return retorno;
 }
