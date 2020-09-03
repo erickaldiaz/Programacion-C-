c7e9c426cf92bdc86483b2c1efb7e969c8bfaec8
erickaldiaz@gmail.com <dificil1>
2020-09-03 03:03:36 -0300

    add min-max with do while

---
+++ min-max-do-while/src/min-max-do-while.c
@@ -0,0 +1,50 @@
+/*
+ ============================================================================
+ Name        : min-max-do-while.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+/*
+ ============================================================================
+ Pedir numeros hasta que el usuario quiera(888) y dar como resultado maximo
+ y minimo
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int main(void) {
+	setbuf(stdout, NULL);
+
+	int maximo;
+	int minimo;
+	int bufferInt;
+	int flag = TRUE;
+
+	do{
+		printf("ingrese un numero entre 0 y 888");
+		scanf("%d", &bufferInt);
+		if(flag == TRUE){
+			maximo = bufferInt;
+			minimo = bufferInt;
+			flag = FALSE;
+		}else{
+			if(bufferInt > maximo){
+				maximo = bufferInt;
+			}if(bufferInt < minimo){
+				minimo = bufferInt;
+			}
+		}
+
+	}
+	while(bufferInt != 888);
+	printf("\n El valor minimo es: %d y el valor máximo es: %d", minimo, maximo);
+	return 0;
+}
